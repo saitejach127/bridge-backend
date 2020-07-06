b59832ce7ccf02e1ee23f35296a4440e87796e93
saitejach127 <saitejach127@gmail.com>
2020-07-06 19:55:33 +0530

    App update added QRCode Scanner

--- Dappconnect/App.js
+++ Dappconnect/App.js
@@ -1,21 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import * as Permissions from "expo-permissions";
+import io from "socket.io-client";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class BarcodeScannerExample extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    scanned: null,
+    openCamera: false,
+    scannedData : null,
+    socket : null
+  };
+
+  componentDidMount() {
+    this.connectToServer();
+    this.getPermissionsAsync();
+  }
+
+  getPermissionsAsync = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      hasCameraPermission: status === "granted",
+    });
+  };
+
+  startScan = () => {
+    console.log("starting");
+    this.setState({ scanned: false });
+  };
+
+  connectToServer = () =>{
+    var socket = io.connect("http://localhost:5000");
+    this.setState({socket});
+  }
+
+  render() {
+    const { hasCameraPermission, scanned } = this.state;
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+
+    if (scanned === false) {
+      return (
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+          style = {
+            StyleSheet.absoluteFillObject
+          }
+        />
+      );
+    }
+
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <Text>{this.state.scannedData}</Text>
+      <Text>{this.state.socket.id}</Text>
+        <Button title="Scan Qr" onPress={this.startScan}></Button>
+      </View>
+    );
+  }
+
+  handleBarCodeScanned = ({ type, data }) => {
+    this.setState({
+      scanned: true,
+      scannedData : data
+    });
+    console.log(this.state.socket);
+    this.state.socket.emit("joinRoom", {id:this.state.scannedData});
+  };
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
